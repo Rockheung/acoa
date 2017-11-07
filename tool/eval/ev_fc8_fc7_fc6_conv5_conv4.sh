8e05f68393fd278fa2fcbd3e20fd7b8f491d9899
Rockheung <rockheung@gmail.com>
2017-11-07 14:35:22 +0900

    fix eval sh

--- tool/eval/ev_fc8_fc7_fc6_conv5_conv4.sh
+++ tool/eval/ev_fc8_fc7_fc6_conv5_conv4.sh
@@ -1,14 +1,14 @@
-ACOASET=/home/rockheung/acoa_dataset
+ACOASET=$HOME/acoa_dataset
 DATASET_DIR=${ACOASET}/dataset
 TRAIN_DIR=${ACOASET}/train_fc8_fc7_fc6_conv5_conv4
 EVAL_DIR=${ACOASET}/eval
 CHECKPOINT_PATH=${ACOASET}/checkpoints/vgg_16.ckpt
 CKPT=${ACOASET}/train_fc8/ckpt-291715
 #CKPT=${ACOASET}/train_full/ckpt55
-SLIM_PATH=/home/rockheung/Downloads/models/research/slim
+SLIM_PATH=$HOME/Downloads/acoa/slim
 python ${SLIM_PATH}/eval_image_classifier.py \
     --dataset_dir=${DATASET_DIR} \
-    --dataset_name=apparel \
+    --dataset_name=acoa \
     --dataset_split_name=validation \
     --model_name=vgg_16 \
     --checkpoint_path=${TRAIN_DIR} \
