20c6ba4e1e07f66c4043ae8a44b1501b60e2c73c
Rockheung <rockheung@gmail.com>
2017-11-10 22:16:20 +0900

    fix training

--- slim_v2/train_image_classifier.py
+++ slim_v2/train_image_classifier.py
@@ -449,27 +449,6 @@ def main(_):
         weight_decay=FLAGS.weight_decay,
         is_training=True)
 
-    ############################ACOA################################
-    #Label conversion following the FLAGS.hierarchy_level
-
-    #if hierarchy_level was chosen as 1, our label(fine grained) 
-    # will be translated to level 1 class(coarse grained) 
-    condition = tf.constant(FLAGS.hierarchy_level)
-
-    keys = tf.constant(lv2_id_to_lv1_id.keys(), dtypes.int64)
-    values = tf.constant(lv2_id_to_lv1_id.values(), dtypes.int64)
-
-    # a hash table is defined for translating
-    table = tf.contrib.lookup.HashTable(
-    tf.contrib.lookup.KeyValueTensorInitializer(keys, values, dtypes.int64, dtypes.int64), -1
-    )
-    out = table.lookup(label)
-
-    #conditional operator in tensorflow
-    label = tf.cond(tf.equal(condition, tf.constant(1)), lambda : out, lambda : label)
-
-    ################################################################
-
     #####################################
     # Select the preprocessing function #
     #####################################
@@ -503,6 +482,27 @@ def main(_):
           labels, dataset.num_classes - FLAGS.labels_offset)
       batch_queue = slim.prefetch_queue.prefetch_queue(
           [images, labels], capacity=2 * deploy_config.num_clones)
+
+    ############################ACOA################################
+    #Label conversion following the FLAGS.hierarchy_level
+
+    #if hierarchy_level was chosen as 1, our label(fine grained) 
+    # will be translated to level 1 class(coarse grained) 
+    condition = tf.constant(FLAGS.hierarchy_level)
+
+    keys = tf.constant(lv2_id_to_lv1_id.keys(), dtypes.int64)
+    values = tf.constant(lv2_id_to_lv1_id.values(), dtypes.int64)
+
+    # a hash table is defined for translating
+    table = tf.contrib.lookup.HashTable(
+    tf.contrib.lookup.KeyValueTensorInitializer(keys, values, dtypes.int64, dtypes.int64), -1
+    )
+    out = table.lookup(label)
+
+    #conditional operator in tensorflow
+    label = tf.cond(tf.equal(condition, tf.constant(1)), lambda : out, lambda : label)
+
+    ################################################################
 
 
     ####################
