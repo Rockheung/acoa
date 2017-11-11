d83f5c90fa80e8425c610547e03b8a255d6b10f1
Rockheung <rockheung@gmail.com>
2017-11-11 14:43:31 +0900

    fix train.py

--- slim_v2/train_image_classifier.py
+++ slim_v2/train_image_classifier.py
@@ -633,7 +633,7 @@ def main(_):
         save_summaries_secs=FLAGS.save_summaries_secs,
         save_interval_secs=FLAGS.save_interval_secs,
         sync_optimizer=optimizer if FLAGS.sync_replicas else None,
-        session_config=config if FLAGS.per_process_gpu_memory_fraction else None)
+        session_config=config)
 
 
 if __name__ == '__main__':
