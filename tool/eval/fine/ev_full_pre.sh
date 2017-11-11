38082c7e3b0c18fd111eb75707cf4b64af5366cb
Rockheung <rockheung@gmail.com>
2017-11-11 17:28:53 +0900

    modified:   eval/fine/ev_full_pre.sh

--- tool/eval/fine/ev_full_pre.sh
+++ tool/eval/fine/ev_full_pre.sh
@@ -15,5 +15,5 @@ python ${SLIM_PATH}/eval_image_classifier.py \
     --eval_dir=${EVAL_DIR} \
     --batch_size 8 \
     --eval_interval_secs 60 \
-    --hierarchy_level 22 \
+    --hierarchy_level 2 \
     --per_process_gpu_memory_fraction 0.9
