f1bb0213bf129d540029d6a505178882116f4cd8
Rockheung <rockheung@gmail.com>
2017-11-11 17:16:05 +0900

    split sh coarse & fine

---
+++ tool/eval/fine/ev_fc8.sh
@@ -0,0 +1,19 @@
+ACOASET=$HOME/acoa_dataset
+DATASET_DIR=${ACOASET}/dataset/class_7
+TRAIN_DIR=${ACOASET}/train_fc8
+EVAL_DIR=${ACOASET}/eval_fc8
+CHECKPOINT_PATH=${ACOASET}/checkpoints/vgg_16.ckpt
+CKPT=${ACOASET}/train_fc8/ckpt-291715
+#CKPT=${ACOASET}/train_full/ckpt55
+SLIM_PATH=$HOME/Downloads/acoa/slim_v2
+python ${SLIM_PATH}/eval_image_classifier.py \
+    --dataset_dir=${DATASET_DIR} \
+    --dataset_name=acoa \
+    --dataset_split_name=validation \
+    --model_name=vgg_16 \
+    --checkpoint_path=${TRAIN_DIR} \
+    --eval_dir=${EVAL_DIR} \
+    --batch_size 8 \
+    --eval_interval_secs 60 \
+    --hierarchy_level 1 \
+    --per_process_gpu_memory_fraction 0.9
---
+++ tool/train/fine/tr_full_pre.sh
@@ -0,0 +1,22 @@
+ACOASET=$HOME/acoa_dataset
+DATASET_DIR=${ACOASET}/dataset/class_7
+TRAIN_DIR=${ACOASET}/train_full_pre
+CHECKPOINT_PATH=${ACOASET}/checkpoints/vgg_16.ckpt
+SLIM_PATH=$HOME/Downloads/acoa/slim_v2
+python ${SLIM_PATH}/train_image_classifier.py \
+    --train_dir=${TRAIN_DIR} \
+    --dataset_dir=${DATASET_DIR} \
+    --dataset_name=acoa \
+    --dataset_split_name=train \
+    --model_name=vgg_16 \
+    --checkpoint_path=${CHECKPOINT_PATH} \
+    --batch_size=32 \
+    --save_interval_secs=1800 \
+    --save_summaries_secs=60 \
+    --learning_rate_decay_factor=0.95 \
+    --weight_decay=0.00004 \
+    --log_every_n_steps=10 \
+    --optimizer=adam \
+    --checkpoint_exclude_scopes=vgg_16/fc8 \
+    --per_process_gpu_memory_fraction=0.5 \
+    --hierarchy_level=1
