871dcd433e7970ec09cdec3a84e8b49a907a4b16
Rockheung <rockheung@gmail.com>
2017-11-12 12:33:55 +0900

    add tb.sh

--- tool/tb_ev.sh
+++ tool/tb_ev.sh
@@ -1,7 +1,7 @@
 ACOASET=$HOME/acoa_dataset
 DATASET_DIR=${ACOASET}/dataset
-TRAIN_DIR=${ACOASET}/train_fc8
-EVAL_DIR=${ACOASET}/eval_fc8
+TRAIN_DIR=${ACOASET}/train_fc8_fc7_fc6_conv5_conv4
+EVAL_DIR=${ACOASET}/eval_fc8_fc7_fc6_conv5_conv4
 CHECKPOINT_PATH=${ACOASET}/checkpoints/vgg_16.ckpt
 CKPT=${ACOASET}/ckpt50
 SLIM_PATH=$HOME/Downloads/acoa/slim
