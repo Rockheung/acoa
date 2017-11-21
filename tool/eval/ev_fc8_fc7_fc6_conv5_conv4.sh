123ae32a989da7321c71fdc348dd52cf870b61aa
Rockheung <rockheung@gmail.com>
2017-11-21 13:22:23 +0900

    eval update

---
+++ tool/eval/ev_fc8_fc7_fc6_conv5_conv4.sh
@@ -0,0 +1,18 @@
+ACOASET=$HOME/acoa_dataset
+DATASET_DIR=${ACOASET}/dataset/$1
+TRAIN_DIR=$3
+EVAL_DIR=${PWD}
+CHECKPOINT_PATH=${ACOASET}/checkpoints/vgg_16.ckpt
+CKPT=${ACOASET}/train_fc8/ckpt-291715
+#CKPT=${ACOASET}/train_full/ckpt55
+SLIM_PATH=$HOME/Downloads/acoa/slim_v2
+python ${SLIM_PATH}/eval_image_classifier.py \
+    --dataset_dir=${DATASET_DIR} \
+    --dataset_name=acoa \
+    --dataset_split_name=validation \
+    --model_name=vgg_16 \
+    --checkpoint_path=${TRAIN_DIR} \
+    --eval_dir=${EVAL_DIR} \
+    --batch_size 16 \
+    --eval_interval_secs 60 \
+    --hierarchy_level $2
